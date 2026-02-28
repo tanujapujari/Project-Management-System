2f7511e99756ecae59e957404437cf71611ff94c
Tanuja Pujari <tanujapujari2004@gmail.com>
2026-02-28 21:45:23 +0530

    add edited json filess

--- PMS-backend/obj/Debug/net8.0/ProjectManagementSystem.AssemblyInfo.cs
+++ PMS-backend/obj/Debug/net8.0/ProjectManagementSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6ebfa11d48c0452463d96898f392db29196f549")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+305dbf235dd292d4cf789bf118376e02fa517247")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
